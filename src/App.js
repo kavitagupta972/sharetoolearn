587a0d87f8e1334824e4f82341fb2887f42ef5f5
kavitagupta972 <kavitagupta972@gmail.com>
2021-10-28 00:37:11 +0530

    Initial draft

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Share Too Learn</h1>
+        <h2> Sharing knowledge to learn new things....</h2>
+        <h4> More interesting contents are coming soon..... Stay tuned....</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
